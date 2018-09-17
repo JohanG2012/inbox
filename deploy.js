ffb7d14ea5c2209c3309e3eb62cd061995db42b1
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-09-17 12:44:55 +0200

    Add infure deploy script

---
+++ deploy.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  process.env.SEED,
+  `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: '0x' + bytecode,
+      arguments: ['Hi there']
+    })
+    .send({ gas: '7000000', from: accounts[0] });
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
